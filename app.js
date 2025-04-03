3d88994a2ab6e5e8124f6dde97bdfc31112b3adc
Grs2080w <grs_santos2019@outlook.com>
2025-04-03 13:57:23 -0300

    healthcheck

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 /* eslint-disable no-undef */
-const express = require("express")
+const express = require('express')
 const app = express()
 
 // get the port from env variable
 /* eslint-disable no-console */
 const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 
-app.get("/version", (req, res) => {
-	res.send("with test berfore deploy")
+app.get('/version', (req, res) => {
+  res.send('with test berfore deploy')
 })
 
-app.get("/healthz", (req, res) => {
-	if (true) {
-		throw new Error("error")
-	}
+app.get('/healthz', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
 
-	res.send("ok")
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
-	console.log(`server started on port ${PORT}`)
+  console.log(`server started on port ${PORT}`)
 })
