144d7237b91d27202eaf929ba84a693a75d6c7d7
Grs2080w <grs_santos2019@outlook.com>
2025-04-03 16:15:36 -0300

    healthcheck

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static("dist"))
 
 app.get("/version", (req, res) => {
-	res.send("with test berfore deploy")
+	res.send("with bad app deployed")
 })
 
 app.get("/healthz", (req, res) => {
