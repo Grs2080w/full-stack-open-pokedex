eae6d0055f187798b238a8e047f540266c6ebe9b
Grs2080w <grs_santos2019@outlook.com>
2025-07-21 11:19:57 -0300

    test to deploy

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ app.get("/version", (req, res) => {
 })
 
 app.get("/healthz", (req, res) => {
-	// eslint-disable-next-line no-constant-condition
-	//if (true) throw 'error...  '
-  
 	res.send("ok")
 })
 
